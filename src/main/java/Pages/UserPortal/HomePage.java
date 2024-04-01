77371d4fd2eb436c987c739114eba04ed79e5bd2
Abdulrahman <emadabdo258@gmail.com>
2024-04-01 11:48:25 +0200

    "changes"

--- src/main/java/Pages/UserPortal/HomePage.java
+++ src/main/java/Pages/UserPortal/HomePage.java
@@ -14,14 +14,14 @@ public class HomePage {
         this.driver = driver;
     }
     Label greetingMSG=new Label(By.xpath("//span[contains(@class,'greeting')]"));
-    Button addButton=new Button(By.xpath("//div[2]/chk-button/button"));
+    Button addButton=new Button(By.xpath("/html/body/chk-root/div/div[2]/chk-header/nav/div[2]/div[2]/chk-button/button/span"));
     Link taskIcon=new Link(By.xpath("//*[@id=\"sidebar\"]/ul/li[7]/div[1]/a[2]"));
     public boolean getGreetingLabel() throws InterruptedException {
         Thread.sleep(2000);
         return greetingMSG.isDisplayed(By.xpath("//span[contains(@class,'greeting')]"));
     }
-    public AddNewReportPage openForm(){
-        addButton.waitImplicitly(5);
+    public AddNewReportPage openForm() throws InterruptedException {
+        Thread.sleep(10000);
         addButton.click();
         return new AddNewReportPage(driver);
     }
